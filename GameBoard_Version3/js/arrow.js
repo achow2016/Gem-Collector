6137d0970670f32abea85ebe6d8f7e2f37f4b94e
Ebon <jay0000015@yahoo.com.tw>
2016-05-16 14:35:08 -0700

    Change pictures' path, try to load the pictures on server

--- GameBoard_Version3/js/arrow.js
+++
@@ -1,8 +0,0 @@
-
-function arrow() {
-    var tempIdLeft = "cell" + chosenCell + "Left";
-    var leftArrow = document.getElementById(tempIdLeft).src = "Pictures/arrowLeft.png";
-    var tempIdRight = "cell" + chosenCell + "Right";
-    var rightArrow = document.getElementById(tempIdRight).src = "Pictures/arrowRight.png";
-    rightArrow = document.getElementById(tempIdRight).attachEvent('on', 'click', sign = 1);
-}
