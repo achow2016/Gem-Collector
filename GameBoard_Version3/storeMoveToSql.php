46735976a98a2f8d76ecaf6183f8f532cdbcd8d1
achow2016 <tilldusk@gmail.com>
2016-05-18 16:19:34 -0700

    Will now push array VAR to php file, where it will be written to the SQL table.
    The last ID (turn id) is echoed back to JS when server and writing operations
    are done and ready.
    
    Finally, JS processes messy raw data into Global integer variable "moveId" for processing in next function.

--- GameBoard_Version3/storeMoveToSql.php
+++ GameBoard_Version3/storeMoveToSql.php
@@ -3,42 +3,68 @@
 This php script receives string data from gameinit.html,
 should process string into array for further use
 */
+//Gets RAW url from $_POST
+$rawdata = var_export($_POST, true);
 
-$pass = file_get_contents("php://input");//pass is now string from JS.
-$pass = json_decode($pass);//pass is now php array.
+//Extract cell number set, seperated by commas.
+$textarray = substr($rawdata,25,-4);
 
+//Remove all commas.
+$textarray = str_replace(',', '', $textarray);
+
+//Split number string into associative PHP array.
+$cleanarray = str_split($textarray);
+
+//Split associative array into indexed array.
+$pass = array_values($cleanarray);
 
 /*
-Secure file to connect to database.
+connect to database.
 */
 
-require_once('./private/connection.php');
+$servername = "mysql7.000webhost.com";
+$username = "a1753342_user";
+$password = "bladeands0ul";
+$dbname = "a1753342_main";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection.
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 /*
 Insert data from array into SQL. Don't need to specify columns since 
 whole row is populated except ID. 
 $result call with requireonce uses secure credentials to access database.
 */
-
-$sql = "INSERT INTO gameboard VALUES ($pass[0],$pass[1],$pass[2],$pass[3],$pass[4],
+$sql = "INSERT INTO gameboard (cell1,cell2,cell3,cell4,cell5,cell6,	cell7,cell8,
+cell9,cell10,cell11,cell12) VALUES ($pass[0],$pass[1],$pass[2],$pass[3],$pass[4],
 $pass[5],$pass[6],$pass[7],$pass[8],$pass[9],$pass[10],$pass[11])";
 
-$result = $connection->query($sql);
+//Check writing to SQL.
+
+if (mysqli_query($conn, $sql)) {
+    echo "Created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 /*
 Retreive last ID of table to check if next move is to be made. 
 ID should be stored as global variable back
 at client browser JS.
 */
-$last_id = mysqli_insert_id($connection);
+$last_id = mysqli_insert_id($conn);
 
 /*
 Send last ID to client-side. Record Last ID in JS and poll
 (interval) to check if next move can be made.
 */
-
-echo $last_id; 
-
+ 
+echo $last_id;
 ?>
 
 <!DOCTYPE html>
