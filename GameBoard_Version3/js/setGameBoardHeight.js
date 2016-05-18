e3dcd2ecdfb12bbb6c747e0f6146a06e1ff36fd4
Ebon <jay0000015@yahoo.com.tw>
2016-05-18 13:17:36 -0700

    Temp removed resize function from the game board

--- GameBoard_Version3/js/setGameBoardHeight.js
+++ GameBoard_Version3/js/setGameBoardHeight.js
@@ -1,6 +1,14 @@
 function setGameBoardHeight() {
     var h = window.innerHeight;
-    document.getElementById('OppntLine').style.height = (h * 0.25) + "px";
-    document.getElementById('GameBoardLine').style.height = (h * 0.5) + "px";
-    document.getElementById('UserLine').style.height = (h * 0.25) + "px";
+    var w = window.innerWidth;
+    if (w < 1000 && h > 500) {
+        document.getElementById('OppntLine').style.width = w + "px";
+        document.getElementById('GameBoardLine').style.width = w + "px";
+        document.getElementById('UserLine').style.width = w + "px";
+    } else if (w > 1000 && h < 500) {
+        document.getElementById('OppntLine').style.height = (h * 0.25) + "px";
+        document.getElementById('GameBoardLine').style.height = (h * 0.5) + "px";
+        document.getElementById('UserLine').style.height = (h * 0.25) + "px";
+    }
+    
 }
