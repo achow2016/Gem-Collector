14a17478a2f93607d2a10b0c0c5a3b8424f185f6
Ebon <jay0000015@yahoo.com.tw>
2016-05-19 00:04:17 -0700

    Modify Home page

--- GameBoard_Version3/js/Eggs.js
+++ GameBoard_Version3/js/Eggs.js
@@ -1,26 +1,34 @@
 function Q1check() {
-         $("#UserRight").css("background-image", "url(pictures/Q.png)");
-         $("#UserRight img").css("display", "none");
-         $("#OppntLeft").css("background-image", "url(pictures/A.png)");
-         $("#OppntLeft img").css("display", "none");
+         $("#UserRight1").css("background-image", "url(pictures/Q.png)");
+         $("#UserRight1 img").css("display", "none");
+         $("#OppntLeft2").css("background-image", "url(pictures/A.png)");
+         $("#OppntLeft2 img").css("display", "none");
      }
 
      function Q1uncheck() {
-         $("#UserRight").css("background-image", "url()");
-         $("#UserRight img").css("display", "initial");
-         $("#OppntLeft").css("background-image", "url()");
-         $("#OppntLeft img").css("display", "initial");
+         $("#UserRight1").css("background-image", "url()");
+         $("#UserRight1 img").css("display", "initial");
+         $("#OppntLeft2").css("background-image", "url()");
+         $("#OppntLeft2 img").css("display", "initial");
      }
      function Q2check() {
-         $("#OppntRight").css("background-image", "url(pictures/Q2.png)");
-         $("#OppntRight img").css("display", "none");
-         $("#UserLeft").css("background-image", "url(pictures/A2.png)");
-         $("#UserLeft img").css("display", "none");
+         $("#OppntRight1").css("background-image", "url(pictures/newQ2Left.png)");
+         $("#OppntRight1 img").css("display", "none");
+         $("#OppntRight2").css("background-image", "url(pictures/newQ2Right.png)");
+         $("#OppntRight2 img").css("display", "none");
+         $("#UserLeft1").css("background-image", "url(pictures/newA2Left.png)");
+         $("#UserLeft1 img").css("display", "none");
+         $("#UserLeft2").css("background-image", "url(pictures/newA2Right.png)");
+         $("#UserLeft2 img").css("display", "none");
      }
 
      function Q2uncheck() {
-         $("#OppntRight").css("background-image", "url()");
-         $("#OppntRight img").css("display", "initial");
-         $("#UserLeft").css("background-image", "url()");
-         $("#UserLeft img").css("display", "initial");
+         $("#OppntRight1").css("background-image", "url()");
+         $("#OppntRight1 img").css("display", "initial");
+         $("#OppntRight2").css("background-image", "url()");
+         $("#OppntRight2 img").css("display", "initial");
+         $("#UserLeft1").css("background-image", "url()");
+         $("#UserLeft1 img").css("display", "initial");
+         $("#UserLeft2").css("background-image", "url()");
+         $("#UserLeft2 img").css("display", "initial");
      } 
