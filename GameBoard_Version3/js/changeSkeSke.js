ce0a2f65c6ead5660d833846db78b37139879641
Ebon <jay0000015@yahoo.com.tw>
2016-05-20 10:56:10 -0700

    Update Skeske's pictures

--- GameBoard_Version3/js/changeSkeSke.js
+++ GameBoard_Version3/js/changeSkeSke.js
@@ -3,10 +3,10 @@ function changeSkeSke() {
     var skeske = document.getElementById('SkeSke');
 
     if (skeskeCount == 0) {
-        skeske.src = "pictures/skeskeSleep.png";
+        skeske.src = "pictures/skeleton10gifoff_s.png";
         skeskeCount = 1;
     }  else if (skeskeCount == 1) {
-        skeske.src = "pictures/skeske.png";
+        skeske.src = "pictures/skeleton10gifOn_s.gif";
         skeskeCount = 0;
     }
 }
