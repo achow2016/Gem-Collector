5c98d22c31101d7c3188005c33e1bd63d78be616
Ebon <jay0000015@yahoo.com.tw>
2016-05-24 11:27:47 -0700

    Update Skeske function

--- Webpage/js/changeSkeSke.js
+++ Webpage/js/changeSkeSke.js
@@ -3,10 +3,10 @@ function changeSkeSke() {
     var skeske = document.getElementById('SkeSke');
 
     if (skeskeCount == 0) {
-        skeske.src = "pictures/skeleton10gifOn_s.gif";
+        skeske.src = "pictures/skeleton10gifoff_s.gif";
         skeskeCount = 1;
     }  else if (skeskeCount == 1) {
-        skeske.src = "pictures/skeleton10gifoff_s.png";
+        skeske.src = "pictures/skeleton10gifOn_s.png";
         skeskeCount = 0;
     }
 }
