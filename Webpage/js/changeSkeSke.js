171ab2fbcce9cc35c29912690dee35f965d53c89
Ebon <jay0000015@yahoo.com.tw>
2016-05-26 13:44:24 -0700

    Modify background music function

--- Webpage/js/changeSkeSke.js
+++ Webpage/js/changeSkeSke.js
@@ -1,4 +1,12 @@
 var skeskeCount = 0;
+
+function setBackMusic() {
+    var backgroundMusicVolume = document.getElementById('backgroundMusic');
+    var backMusic = new Audio('music/bensound-ukulele.mp3')
+    backMusic.play();
+    backgroundMusicVolume.volume = 0.75;
+}
+
 function changeSkeSke() {
     var skeske = document.getElementById('SkeSke');
     var backgroundMusicVolume = document.getElementById('backgroundMusic');
@@ -9,7 +17,7 @@ function changeSkeSke() {
         skeskeCount = 1;
     }  else if (skeskeCount == 1) {
         skeske.src = "pictures/skeleton10gifOn_s.gif";
-        backgroundMusicVolume.volume = 1.0;
+        backgroundMusicVolume.volume = 0.75;
         skeskeCount = 0;
     }
 }
