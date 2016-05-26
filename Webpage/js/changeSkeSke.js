de33606ae28afaf653dcdf09082118c6662e98f0
Ebon <jay0000015@yahoo.com.tw>
2016-05-26 12:08:30 -0700

    Embed background music in pages

--- Webpage/js/changeSkeSke.js
+++ Webpage/js/changeSkeSke.js
@@ -1,12 +1,15 @@
 var skeskeCount = 0;
 function changeSkeSke() {
     var skeske = document.getElementById('SkeSke');
+    var backgroundMusicVolume = document.getElementById('backgroundMusic');
 
     if (skeskeCount == 0) {
-        skeske.src = "pictures/skeleton10gifOn_s.gif";
+        skeske.src = "pictures/skeleton10gifoff_s.gif";
+        backgroundMusicVolume.volume = 0.0;
         skeskeCount = 1;
     }  else if (skeskeCount == 1) {
-        skeske.src = "pictures/skeleton10gifoff_s.png";
+        skeske.src = "pictures/skeleton10gifOn_s.gif";
+        backgroundMusicVolume.volume = 1.0;
         skeskeCount = 0;
     }
 }
